3a0d0917b77d78a61d94779b52d1fd71d7b439e3
chukwu chukwuebuka david <64756965+davidchukwu202@users.noreply.github.com>
2024-02-11 17:09:31 +0100

    Update Credit card.js

--- Credit card.js
+++ Credit card.js
@@ -1,24 +1,17 @@
-function validateCreditCard(cardNumber) {
-    // Regular expressions for different credit card types
-    const visaRegex = /^4[0-9]{12}(?:[0-9]{3})?$/;
-    const mastercardRegex = /^5[1-5][0-9]{14}$/;
-    const amexRegex = /^3[47][0-9]{13}$/;
-    const discoverRegex = /^6(?:011|5[0-9]{2})[0-9]{12}$/;
+function CreditCard(cardNumber) {
+    // Regular expressions
+    const visa= /^4[0-9]{12}(?:[0-9]{3})?$/;
+    const mastercard= /^5[1-5][0-9]{14}$/;
 
-    // Check if the card number matches any of the regex patterns
-    if (visaRegex.test(cardNumber)) {
+    // Check if the card number matches
+    if (visa.test(cardNumber)) {
         return "Visa";
-    } else if (mastercardRegex.test(cardNumber)) {
+    } else if (mastercard.test(cardNumber)) {
         return "Mastercard";
-    } else if (amexRegex.test(cardNumber)) {
-        return "American Express";
-    } else if (discoverRegex.test(cardNumber)) {
-        return "Discover";
     } else {
         return "Invalid card type";
     }
 }
 
-// Example usage:
-const cardNumber = "4111111111111111"; // Change this to test different card numbers
-console.log(validateCreditCard(cardNumber));
+const cardNumber = "5111222233331111";
+console.log(CreditCard(cardNumber));
